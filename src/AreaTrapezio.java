ef2a2e85a5b2e5241b35671c969168d60a6ec994
leandro <leandrolacerda.sistema@gmail.com>
2025-05-23 21:58:22 -0300

    Classe AreaTrapezio

---
+++ src/AreaTrapezio.java
@@ -0,0 +1,13 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class AreaTrapezio {
+    public static void main(String[] args) {
+
+        double b, B, h, area;
+        b = 6.0;
+        B = 8.0;
+        h = 5.0;
+        area = (b + B) / 2 * h;
+        System.out.println("A area do trapezio é:  " + area);
+    }
+}
