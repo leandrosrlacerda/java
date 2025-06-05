713664087761dc7ba4d562daf2f106f6bf9b3512
leandro <leandrolacerda.sistema@gmail.com>
2025-06-04 21:36:44 -0300

    Change in the class Polimorfismo

---
+++ src/Polimorfismo.java
@@ -0,0 +1,85 @@
+public class Polimorfismo {
+
+    public static class PersonalityObject {
+        public String speak() {
+            return "I am an Object.";
+        }
+
+        public void makeSpeak(PersonalityObject obj) {
+            System.out.println(obj.speak());
+        }
+
+    }
+
+    public static class PessimisticObject extends PersonalityObject {
+        public String speak() {
+            return "The glass is half empty.";
+        }
+
+        public void makeSpeak(PessimisticObject obj) {
+            System.out.println(obj.speak());
+        }
+    }
+
+    public static class OptimisticObject extends PersonalityObject {
+        public String speak() {
+            return "The glass is half full.";
+        }
+
+        public void makeSpeak(OptimisticObject obj) {
+            System.out.println(obj.speak());
+        }
+    }
+
+    public static class IntrovertedObject extends PersonalityObject {
+        public String speak() {
+            return "hi";
+        }
+
+        public void makeSpeak(IntrovertedObject obj) {
+            System.out.println(obj.speak());
+        }
+    }
+
+    public static class ExtrovertedObject extends PersonalityObject {
+        public String speak() {
+            return "Hello, blah blah blah, did you know that blah blah blah.";
+        }
+
+        public void makeSpeak(ExtrovertedObject obj) {
+            System.out.println(obj.speak());
+        }
+    }
+
+    public static void main(String[] args) {
+
+        PersonalityObject personality = new PersonalityObject();
+        PessimisticObject pessimistic = new PessimisticObject();
+        OptimisticObject optimistic = new OptimisticObject();
+        IntrovertedObject introverted = new IntrovertedObject();
+        ExtrovertedObject extroverted = new ExtrovertedObject();
+
+        // a capacidade de subtituição permite fazer o seguinte
+        PersonalityObject[] personalities = new PersonalityObject[5];
+        personalities[0] = personality;
+        personalities[1] = pessimistic;
+        personalities[2] = optimistic;
+        personalities[3] = introverted;
+        personalities[4] = extroverted;
+
+        int count = 0;
+        for (PersonalityObject p : personalities) {
+            System.out.println("Personalities[" + count + "] speaks: " + p.speak());
+            count++;
+        }
+        System.out.println("");
+
+        PersonalityObject obj = new PersonalityObject();
+        obj.makeSpeak(pessimistic);
+        obj.makeSpeak(optimistic);
+        obj.makeSpeak(introverted);
+        obj.makeSpeak(extroverted);
+
+    }
+
+}
