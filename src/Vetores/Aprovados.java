312dfa3325977880798a645ad7241e6ff8b18c5e
leandro <leandrolacerda.sistema@gmail.com>
2025-09-02 13:31:48 -0300

    Classes Aluno, Aprovados, Pessoa

---
+++ src/Vetores/Aprovados.java
@@ -0,0 +1,35 @@
+package Vetores;
+
+import java.util.Scanner;
+
+public class Aprovados {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Quantos alunos serão digitados? ");
+        int n = input.nextInt();
+
+        Aluno[] alunos = new Aluno[n];
+
+        input.nextLine();
+
+        for (int i = 0; i < alunos.length; i++) {
+            alunos[i] = new Aluno();
+            System.out.println("Dados do " + (i + 1) + "a aluno: ");
+            alunos[i].setNome(input.nextLine());
+            alunos[i].setNota1(input.nextDouble());
+            alunos[i].setNota2(input.nextDouble());
+            input.nextLine();
+        }
+
+        for (int i = 0; i < alunos.length; i++) {
+            if ((alunos[i].getNota1() + alunos[i].getNota2()) / 2 > 6.0) {
+                System.out.println(alunos[i].getNome() + " foi aprovado!");
+            } else {
+                System.out.println(alunos[i].getNome() + " foi reprovado!");
+            }
+        }
+
+        input.close();
+    }
+}
---
+++ src/Vetores/MaisVelho.java
@@ -0,0 +1,40 @@
+package Vetores;
+
+import java.util.Scanner;
+
+public class MaisVelho {
+
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Quantas pessoas voce vai digitar? ");
+        int n = input.nextInt();
+        int maiorIdade = 0, posicao = 0;
+        Pessoa[] pessoas = new Pessoa[n];
+
+        input.nextLine();
+
+        for (int i = 0; i < pessoas.length; i++) {
+            pessoas[i] = new Pessoa();
+            System.out.println("Dados da " + (i + 1) + "a pessoa: ");
+            System.out.print("Nome: ");
+            pessoas[i].setNome(input.nextLine());
+            System.out.print("Idade: ");
+            pessoas[i].setIdade(input.nextInt());
+            input.nextLine();
+        }
+
+        for (int i = 0; i < pessoas.length; i++) {
+            if(pessoas[i].getIdade() > maiorIdade){
+                maiorIdade = pessoas[i].getIdade();
+                posicao = i;
+            }
+        }
+
+        System.out.println("PESSOA MAIS VELHA: " + pessoas[posicao].getNome());
+        System.out.println("IDADE DA PESSOA MAIS VELHA: " + pessoas[posicao].getIdade());
+
+        input.close();
+    }
+}
